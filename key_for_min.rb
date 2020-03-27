e992aa857d0004d65a635bc1fef530951b925c11
Javante Nelson <javantenelson@gmail.com>
2020-03-27 03:00:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,10 @@ def key_for_min_value(name_hash)
   lowest_value= nil
   lowest_key=nil
   name_hash.each do |thing, number| if
-  lowest_value==nil || number < 
-
+  lowest_value==nil || number < lowest_value
+  lowest_value=number
+  lowest_key=key
+end
+end
+lowest_key
 end
